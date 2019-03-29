3d2e57d08f5f7126ff7974a0e19ffe129ce478a3
Karlo Krakan <karlo@krakan.ca>
2019-03-29 11:41:55 -0700

    some changes

--- src/server.js
+++ src/server.js
@@ -1,54 +1,90 @@
-// REQUIRES
-const lists = require('./js/data');
 const express = require('express');
 const app = express();
 const fs = require("fs");
 const { JSDOM } = require('jsdom');
 const bodyParser = require('body-parser');
+const mysql = require('mysql');
 
 app.use('/html', express.static('html'));
 app.use('/css', express.static('css'));
 app.use('/js', express.static('js'));
 app.use('/resources', express.static('resources'));
 
-//Loads page when launched.
-app.get('/', function (req, res) {
-    let doc = fs.readFileSync('html/re-createdindex.html', "utf8");
-    res.send(doc);
-})
+var db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "lab8"
+});
 
-
-app.get("/ajax-GET-list", function (req, res) {
-
-//    res.setHeader('Content-Type', 'application/json');
-    console.log(req.query['format']);
-    let formatOfResponse = req.query['format'];
-    let dataList = null;
-
-    if(formatOfResponse == 'html-list') {
-
-        res.setHeader('Content-Type', 'text/html');
-        dataList = lists.getHTML();
-        res.send(dataList);
-
-    } else if(formatOfResponse == 'json-list') {
-
-        res.setHeader('Content-Type', 'application/json');
-        dataList = lists.getJSON();
-        res.send(dataList);
-
-    } else {
-        res.send({msg: 'Wrong format!'});
+db.connect(function(err){
+    if (err){
+        console.log(info() + " " + err);
+    }
+    else {
+        console.log(info() + " connected...");
     }
 });
 
-// for page not found (i.e., 404)
+function info() {
+    now = new Date();
+    return now.getTime();
+};
+
+app.get('/', function (req, res) {
+    let doc = fs.readFileSync('html/index.html', "utf8");
+    res.send(doc);
+})
+
+app.get("/ajax-GET-tab-content", function (req, res) {
+    let format = req.query['format'];
+    let tab = req.query['tab'];
+    
+    function getHTML() {
+        res.setHeader('Content-Type', 'text/html');
+        console.log("html requested");
+        var sql = "SELECT * FROM " + tab;
+        db.query(sql, function(err, result, fields) {
+            if (err) {
+                console.log(err);
+                res.send({msg: 'Wrong Format!, or DB error'});
+            }else {
+                console.log(result);
+                let html = '<p>' + result[0]['comment']+ '</p>'
+                res.send(html);
+                console.log("Sent result");
+            }
+        });
+    }
+
+    function getJSON() {
+        res.setHeader('Content-Type', 'application/json');
+        console.log("json requested");
+        var sql = "SELECT * FROM " + tab;
+        db.query(sql, function(err, result, fields) {
+            if (err) {
+                console.log(err);
+                res.send({msg: 'Wrong Format!, or DB error'});
+            }else {
+                console.log(result);
+                res.send(result);
+                console.log("Sent result");
+            }
+        });
+    }
+
+    if (format == 'html') {
+        getHTML();
+    } else if (format == 'json') {
+        getJSON();
+    }
+});
+
 app.use(function (req, res, next) {
   res.status(404).send("<html><head><title>Page not found!</title></head><body><p>Nothing here.</p></body></html>");
     
 })
 
-// RUN SERVER
 let port = 3000;
 app.listen(port, function () {
     console.log('Karlo\'s Gym CLUB ! listening on port ' + port + '!');
