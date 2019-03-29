193a6e0b10c7095285be830737244623b335569c
Karlo Krakan <karlo@krakan.ca>
2019-03-28 23:45:36 -0700

    commit

--- src/js/ajax.js
+++ src/js/ajax.js
@@ -1,109 +1,81 @@
 $(document).ready(function() {
-
-    // Click the facilities tab
-//    $('#facilities').click(function(e) {
-//        // don't allow the anchor to visit the link
-//        e.preventDefault();
-//            
-//        $.ajax({
-//            url: "/ajax-GET-list",
-//            dataType: "html",
-//            type: "GET",
-//            data: {format: 'html-list'},
-//            success: function(data) {
-//                for (i = 1; i <= 10; i++) {
-//                    $("#p" + i).html(data);
-////                let htmlStr = $("#p" + i);
-////                    for (i = 0; i < data.length; i++) {
-////                        htmlStr.text(data[i]);
-////                    }
-//                }console.log("SUCCESS:", data);
-//
-//            },
-//            error: function(jqXHR, textStatus, errorThrown) {
-//                for (i = 1; i < 10; i++){
-//                $("#p" + i).text(jqXHR.statusText);
-//                console.log("ERROR:", jqXHR, textStatus, errorThrown);
-//            }}
-//
-//        });
-//    });
     
-    //            data: { format: "json-list"},
-     //       success: function(data) {
-    //
-    $('#facilities').click(function(e) {
-
-        // don't allow the anchor to visit the link
-        e.preventDefault();
-
+    function ajaxGetFacilities() {
         $.ajax({
-            url: '/get',
+            url: "/ajax-get-tab-content",
             dataType: "html",
             type: "GET",
-            data: { format: "html-list"},
+            data: {format: "html", tab: "facilities"},
             success: function(data) {
+                for(i=1; i<=data.length; i++){
                 console.log("SUCCESS HTML:", data);
-                $("#p1").html(data);
-
+                
+                $("#p" + i).html(data);
+                }
             },
             error: function(jqXHR, textStatus, errorThrown) {
                 $("#p1").text(jqXHR.statusText);
                 console.log("ERROR:", jqXHR, textStatus, errorThrown);
             }
-
         });
-    });
-    // GET A LIST OF 'THINGS' FROM THE SERVER AS JSON DATA
-    $('#reviews').click(function(e) {
-
-        // don't allow the anchor to visit the link
-        e.preventDefault();
-
+    }
+    
+    function ajaxGetReviews() {
         $.ajax({
-            url: '/get',
+            url: "/ajax-get-tab-content",
             dataType: "json",
             type: "GET",
-            data: { format: "json-list"},
+            data: {format: "json", tab: "reviews"},
             success: function(data) {
                 console.log("SUCCESS JSON:", data);
-                for(i = 1; i <= 10; i++) {
-                let htmlStr = $("#p"+i);
-                for(let i = 0; i < data.length; i++) {
-                    htmlStr += "<li>" + data[i] + "</li>";
+                
+                for(i = 0; i<data.length; i++) {
+                    $("#p" + i + 1).append('<p>' + data[i]['comment']+ '</p>');
+                    console.log(data[i]['comment']);
                 }
-                htmlStr += "</ul>";
-                }
+//                let htmlStr = "";
+//                for(let i = 0; i < data.length; i++) {
+//                    htmlStr += "<li>" + data[i] + "</li>";
+//                }
+//                htmlStr += "</ul>";
+//                    htmldiv.html(htmlStr);
+//                }
 
             },
             error: function(jqXHR, textStatus, errorThrown) {
                 for(i =1; i<=10; i++){
-                $("#p"+i).text(jqXHR.statusText);
-                console.log("ERROR:", jqXHR, textStatus, errorThrown);
+                    $("#p" + i).text(jqXHR.jqXHR);
+                    console.log("ERROR:", jqXHR, textStatus, errorThrown);
                 }
             }
         });
+    }
+
+    $('#facilities').click(function(e) {
+        $("#facilities").css({
+            "border-bottom": "2px solid red",
+            "color": "black"
+        });
+        $("#reviews").css({
+            "border-bottom": "0px solid red",
+            "color": "#888888"
+        });
+        ajaxGetFacilities();
     });
     
-        $('#title').click(function(e) {
-
-        // don't allow the anchor to visit the link
-        e.preventDefault();
-
-        $.ajax({
-            url: "/ajax-GET",
-            dataType: "json",
-            type: "GET",
-            success: function(data) {
-                $("#p1").text(data['msg']);
-                console.log("SUCCESS:", data);
-
-            },
-            error: function(jqXHR, textStatus, errorThrown) {
-                $("#p1").text(jqXHR.statusText);
-                console.log("ERROR:", jqXHR, textStatus, errorThrown);
-            }
-
+    // GET A LIST OF 'THINGS' FROM THE SERVER AS JSON DATA
+    $('#reviews').click(function(e) {
+        $("#reviews").css({
+            "border-bottom": "2px solid red",
+            "color": "black"
+        });    
+        $("#facilities").css({
+            "border-bottom": "0px solid red",
+            "color": "#888888"
         });
+        ajaxGetReviews();
     });
+    
+    // On start up
+    ajaxGetReviews();
 });
--- src/server.js
+++ src/server.js
@@ -1,5 +1,3 @@
-// REQUIRES
-const lists = require('./js/data');
 const express = require('express');
 const app = express();
 const fs = require("fs");
@@ -27,56 +25,65 @@ db.connect(function(err){
         console.log(info() + " connected...");
     }
 });
+
 function info() {
     now = new Date();
     return now.getTime();
 };
 
-//Loads page when launched.
 app.get('/', function (req, res) {
-    let doc = fs.readFileSync('html/re-createdindex.html', "utf8");
+    let doc = fs.readFileSync('html/index.html', "utf8");
     res.send(doc);
 })
 
-
-app.get("/ajax-GET-list", function (req, res) {
-
-//    res.setHeader('Content-Type', 'application/json');
-    console.log(req.query['format']);
-    let formatOfResponse = req.query['format'];
-    let dataList = null;
-
-    if(formatOfResponse == 'html-list') {
-
+app.get("/ajax-get-tab-content", function (req, res) {
+    let format = req.query['format'];
+    let tab = req.query['tab'];
+    
+    function getHTML() {
         res.setHeader('Content-Type', 'text/html');
-        dataList = lists.getHTML();
-        res.send(dataList);
-
-    } else if(formatOfResponse == 'json-list') {
-
-        res.setHeader('Content-Type', 'application/json');
-            console.log("reviews requested");
-        var sql = "SELECT * FROM review";
+        console.log("html requested");
+        var sql = "SELECT * FROM " + tab;
         db.query(sql, function(err, result, fields) {
-        if(err) {
-            console.log(err);
-            res.send({msg: 'Wrong Format!, or DB error'});
-        }else {
-            console.log(result);
-            res.send(result);
-            console.log("Sent result");
-        }
-    });
+            if(err) {
+                console.log(err);
+                res.send({msg: 'Wrong Format!, or DB error'});
+            }else {
+                console.log(result);
+                res.send(result);
+                console.log("Sent result");
+            }
+        });
+    }
 
-}});
+    function getJSON() {
+        res.setHeader('Content-Type', 'application/json');
+        console.log("json requested");
+        var sql = "SELECT * FROM " + tab;
+        db.query(sql, function(err, result, fields) {
+            if(err) {
+                console.log(err);
+                res.send({msg: 'Wrong Format!, or DB error'});
+            }else {
+                console.log(result);
+                res.send(result);
+                console.log("Sent result");
+            }
+        });
+    }
 
-// for page not found (i.e., 404)
+    if (format == 'html') {
+        getHTML();
+    } else if(format == 'json') {
+        getJSON();
+    }
+});
+
 app.use(function (req, res, next) {
   res.status(404).send("<html><head><title>Page not found!</title></head><body><p>Nothing here.</p></body></html>");
     
 })
 
-// RUN SERVER
 let port = 3000;
 app.listen(port, function () {
     console.log('Karlo\'s Gym CLUB ! listening on port ' + port + '!');
